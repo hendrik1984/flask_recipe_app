2cd764ab3fde319be1cdc9966cb141149fe8efdc
Hendrik Rahardja <hendrik@sepulsa.com>
2018-02-23 14:42:34 +0700

    added use of instance folder

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,14 @@
+#################
+#### imports ####
+#################
+
 from flask import Flask
 
-app = Flask(__name__)
+################
+#### config ####
+################
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('flask.cfg')
+
 from . import views
